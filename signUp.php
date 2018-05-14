4f13d9e32ea8380aa4fb38b80c6e3dd6e42553e7
ckern-tgm <ckern@student.tgm.ac.at>
2018-05-14 22:53:27 +0200

    git add *

--- signUp.php
+++ signUp.php
@@ -67,13 +67,14 @@ echo "<script type='text/javascript'>alert('$sql');</script>";
 $uname = $_POST['uname'];
 $email = $_POST['email'];
 $psw = $_POST['psw'];
-
-
 $dbconn = pg_connect("host=ec2-23-23-247-245.compute-1.amazonaws.com port=5432 dbname=de8h555uj0b1mq user=xokkwplhovrges password=56a064f11b2b07249b0497b9f3e6e4ee306fc72b24fd469618658c0738e23e7d");
-$sql = "create table passwd (varchar(255));";
-$insert = "INSERT INTO passwd VALUES('psw')"
-$r = pg_query($dbconn, $sql);
-$r = pg_query($dbconn, $insert);
+
+if($_POST){
+	$sql = "create table passwd (varchar(255));";
+	$insert = "INSERT INTO passwd VALUES('psw')"
+	$r = pg_query($dbconn, $sql);
+	$r = pg_query($dbconn, $insert);
+}
 /*
 $tbls = "select * from information_schema.tables";
 $qr = pg_query($dbconn, $tbls);
