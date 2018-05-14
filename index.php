04afcf1f309e21935e794acc87ca51ee46f2b4ad
ckern-tgm <ckern@student.tgm.ac.at>
2018-05-15 00:01:46 +0200

    testing

--- index.php
+++ index.php
@@ -44,8 +44,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['submit']))
 	$getUname = "SELECT uname FROM users WHERE uname = '$uname';";
 	$getPsw = "SELECT passwd FROM users WHERE passwd = '$passwd';";
 	echo "<script type='text/javascript'>alert('$getUname');</script>";
-	echo "<script type='text/javascript'>alert('$uname');</script>";
-	if($getUname == $uname && $getPsw == $psw){
+	if($getUname == $uname){
 		echo "<script type='text/javascript'>alert('success');</script>";
 	}
 	$i = pg_query($dbconn, $insert);
