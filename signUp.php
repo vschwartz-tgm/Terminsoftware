746e3a152f001e40e7401fea2f9fcf94106a58c4
ckern-tgm <ckern@student.tgm.ac.at>
2018-05-14 23:12:57 +0200

    insert values into db

--- signUp.php
+++ signUp.php
@@ -71,12 +71,19 @@ if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['submit']))
 	$psw = $_POST['psw'];
 	$dbconn = pg_connect("host=ec2-23-23-247-245.compute-1.amazonaws.com port=5432 dbname=de8h555uj0b1mq user=xokkwplhovrges password=56a064f11b2b07249b0497b9f3e6e4ee306fc72b24fd469618658c0738e23e7d");
 
-	$sql = "create table passwd (pwd varchar(255));";
+
 	$insert = "INSERT INTO passwd VALUES('psw');";
-	$r = pg_query($dbconn, $sql);
-	$r = pg_query($dbconn, $sql);
+	$i = pg_query($dbconn, $insert);
 	
 }
+
+/*Erstellen der Tabellen (FUNKTIONIERT)
+
+	$sql = "create table passwd (pwd varchar(255));";
+	$r = pg_query($dbconn, $sql);
+
+
+*/
 /*
 $tbls = "select * from information_schema.tables";
 $qr = pg_query($dbconn, $tbls);
