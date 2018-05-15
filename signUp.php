f71008b78539fb45c92e5a890754eefea176a132
Paul Mazzolini <pmazzolini@student.tgm.ac.at>
2018-05-15 08:33:21 +0200

    Echo alert

--- signUp.php
+++ signUp.php
@@ -69,6 +69,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['submit']))
 	$uname = $_POST['uname'];
 	$email = $_POST['email'];
 	$psw = $_POST['psw'];
+	echo "<script type='text/javascript'>alert('Hallo');</script>";
 	$dbconn = pg_connect("host=ec2-23-23-247-245.compute-1.amazonaws.com port=5432 dbname=de8h555uj0b1mq user=xokkwplhovrges password=56a064f11b2b07249b0497b9f3e6e4ee306fc72b24fd469618658c0738e23e7d");
 
 
