7e6c3653f8eb7bcd7b599a4d08c0306a0b958d3b
Paul Mazzolini <pmazzolini@student.tgm.ac.at>
2018-05-15 14:18:16 +0200

    Aufteilen des Quellcodes

--- index.php
+++ index.php
@@ -33,11 +33,17 @@
 </html>
 
 <?php
-	if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['submit']))
-	{
-		$dbconn = pg_connect("host=ec2-23-23-247-245.compute-1.amazonaws.com port=5432 dbname=de8h555uj0b1mq user=xokkwplhovrges password=56a064f11b2b07249b0497b9f3e6e4ee306fc72b24fd469618658c0738e23e7d");
+	if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['submit'])){
 		$uname = $_POST['uname'];
 		$psw = $_POST['psw'];
+		login($uname, $psw);
+	}
+		
+	
+	function login($uname, $psw){
+		$dbconn = pg_connect("host=ec2-23-23-247-245.compute-1.amazonaws.com port=5432 dbname=de8h555uj0b1mq user=xokkwplhovrges password=56a064f11b2b07249b0497b9f3e6e4ee306fc72b24fd469618658c0738e23e7d");
+		/*$uname = $_POST['uname'];
+		$psw = $_POST['psw'];*/
 		$fehler = false; 
 		
 		// Gibt es diesen User?
--- signUp.php
+++ signUp.php
@@ -8,34 +8,35 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<title>Terminreservierung</title>
 	</head>
-<body>
-	<form action="" method="post" style="border:1px solid #ccc">
-	  <div class="container">
-		<h1>Sign Up</h1>
-		<hr>
-		
-		<label for="uname"><b>Username</b></label>
-		<input type="text" placeholder="Enter Username" name="uname" id="uname" required>
+	<body>
+		<form action="" method="post" style="border:1px solid #ccc">
+		  <div class="container">
+			<h1>Sign Up</h1>
+			<hr>
+			
+			<label for="uname"><b>Username</b></label>
+			<input type="text" placeholder="Enter Username" name="uname" id="uname" required>
 
-		<label for="email"><b>Email</b></label>
-		<input type="text" placeholder="Enter Email" name="email" id="email" required>
+			<label for="email"><b>Email</b></label>
+			<input type="text" placeholder="Enter Email" name="email" id="email" required>
 
-		<label for="psw"><b>Password</b></label>
-		<input type="password" placeholder="Enter Password" name="psw" id="psw" required>
+			<label for="psw"><b>Password</b></label>
+			<input type="password" placeholder="Enter Password" name="psw" id="psw" required>
 
-		<label for="psw-repeat"><b>Repeat Password</b></label>
-		<input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required>
+			<label for="psw-repeat"><b>Repeat Password</b></label>
+			<input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required>
 
-		<p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
-		  
-		<div class="clearfix">
-		  <button type="button" class="cancelbtn" onclick ="window.location = 'index.php'">Cancel</button>
-		  <button type="submit" class="signupbtn" name="submit">Sign Up</button>
-		</div>
-	  </div>
-	</form>
-</body>
+			<p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
+			  
+			<div class="clearfix">
+				<button type="button" class="cancelbtn" onclick ="window.location = 'index.php'">Cancel</button>
+				<button type="submit" class="signupbtn" name="submit">Sign Up</button>
+			</div>
+		  </div>
+		</form>
+	</body>
 </html>
+
 <?php
 	/*
 	$host ="ec2-23-23-247-245.compute-1.amazonaws.com";
@@ -44,61 +45,35 @@
 	$dbname = "de8h555uj0b1mq";
 	$port = "5432";
 
-
 	$db = parse_url(getenv("DATABASE_URL"));
 	$db["path"] = ltrim($db["path"], "/");
-
 	$conn = pg_connect(getenv("DATABASE_URL"));
-
-
+	
 	if($_POST){
 	$sql = 'SELECT * FROM users';
 
-<<<<<<< HEAD
-echo "<script type='text/javascript'>alert('$sql');</script>";
-}
-*/
-
-if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['submit']))
-    {
-	$uname = $_POST['uname'];
-	$email = $_POST['email'];
-	$psw = $_POST['psw'];
-	register();
-	
-	}
-function register($uname, $email, $psw){
-	$dbconn = pg_connect("host=ec2-23-23-247-245.compute-1.amazonaws.com port=5432 dbname=de8h555uj0b1mq user=xokkwplhovrges password=56a064f11b2b07249b0497b9f3e6e4ee306fc72b24fd469618658c0738e23e7d");
-
-	$pswrepeat = $_POST['psw-repeat'];
-	$fehler = false; 
-
-
-	// Uname schon vorhanden?
-	$slct = "SELECT COUNT(*) FROM users WHERE uname = '".$uname."';"; 
-	$sql = pg_query($dbconn, $slct); 
-	$row = pg_fetch_row($sql); 
-	if($row[0] > 0) { 
-		$fehler = true;
-		echo "<script type='text/javascript'>alert('Dieser User existiert bereits!');</script>";
-=======
 	echo "<script type='text/javascript'>alert('$sql');</script>";
->>>>>>> 019a02fca375c5b7d6cf3fd1d9be7d08a3b68f99
 	}
 	*/
 
-	if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['submit']))
-    {
-		$dbconn = pg_connect("host=ec2-23-23-247-245.compute-1.amazonaws.com port=5432 dbname=de8h555uj0b1mq user=xokkwplhovrges password=56a064f11b2b07249b0497b9f3e6e4ee306fc72b24fd469618658c0738e23e7d");
-		
+	if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['submit'])){
 		$uname = $_POST['uname'];
 		$email = $_POST['email'];
 		$psw = $_POST['psw'];
-
 		$pswrepeat = $_POST['psw-repeat'];
+		register($uname, $email, $psw, $pswrepeat);
+	}
+	
+	
+	function register($uname, $email, $psw, $pswrepeat){
+		$dbconn = pg_connect("host=ec2-23-23-247-245.compute-1.amazonaws.com port=5432 dbname=de8h555uj0b1mq user=xokkwplhovrges password=56a064f11b2b07249b0497b9f3e6e4ee306fc72b24fd469618658c0738e23e7d");
+		
+		/*$uname = $_POST['uname'];
+		$email = $_POST['email'];
+		$psw = $_POST['psw'];
+		$pswrepeat = $_POST['psw-repeat'];*/
 		$fehler = false; 
-
-
+		
 		// Uname schon vorhanden?
 		$slct = "SELECT COUNT(*) FROM users WHERE uname = '".$uname."';"; 
 		$sql = pg_query($dbconn, $slct); 
@@ -107,8 +82,7 @@ function register($uname, $email, $psw){
 			$fehler = true;
 			echo "<script type='text/javascript'>alert('Dieser User existiert bereits!');</script>";
 		}
-
-			
+		
 		// Email schon vorhanden?
 		if ($fehler == false){
 			$sql = "SELECT COUNT(*) FROM users WHERE email = '".$email."';"; 
@@ -119,18 +93,18 @@ function register($uname, $email, $psw){
 				echo "<script type='text/javascript'>alert('Diese Email hat bereits einen Account!');</script>";
 			}
 		}
-
+		
 		// Email möglich?
 		if ($fehler == false){
 			if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
 				// Kein Fehler
 			} else {
 				$fehler = true;
-				echo "<script type='text/javascript'>alert('Diese Email existiert nicht!');</script>";
+				echo "<script type='text/javascript'>alert('Ungültige Emailadresse!');</script>";
 			}
 		}
-
-		// Passwort gleich?
+		
+		// Passwörter gleich?
 		if ($fehler == false){
 			if ($psw != $pswrepeat){
 				echo "<script type='text/javascript'>alert('Passwörter stimmen nicht überein!');</script>";
@@ -144,40 +118,37 @@ function register($uname, $email, $psw){
 			$i = pg_query($dbconn, $insert);
 			header("Location: index.php");
 		}
-		
-		
-		/*
-			Erstellen der Tabellen (FUNKTIONIERT)
-
-			$sql = "create table passwd (pwd varchar(255));";
-			$r = pg_query($dbconn, $sql);
-
-			$tbls = "select * from information_schema.tables";
-			$qr = pg_query($dbconn, $tbls);
-			while($r = pg_fetch_array($qr)) {
-				echo "<script type='text/javascript'>alert('$r');</script>";
-			}
-		*/
 	}
 ?>
 
 <?php
-
 /*
-Funktioniert lokal aber nicht mit Heroku da schreibrechte Fehlen.
- if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['submit']))
-    {
-        saveData();
-    }
+	Erstellen der Tabellen (FUNKTIONIERT)
 
-function saveData(){
-	$file = '/home/christoph/Documents/text.txt';
-	// Öffnet die Datei, um den vorhandenen Inhalt zu laden
-	$current = file_get_contents($file);
-	$myfile = 'login.txt';
-	$txt = 	$_POST['uname'];
-	file_put_contents($file, $txt);
-	echo "<script type='text/javascript'>alert('$current');</script>";
-}
+	$sql = "create table passwd (pwd varchar(255));";
+	$r = pg_query($dbconn, $sql);
+
+	$tbls = "select * from information_schema.tables";
+	$qr = pg_query($dbconn, $tbls);
+	while($r = pg_fetch_array($qr)) {
+		echo "<script type='text/javascript'>alert('$r');</script>";
+	}
+*/
+/*
+	Funktioniert lokal aber nicht mit Heroku da schreibrechte Fehlen.
+	 if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['submit']))
+		{
+			saveData();
+		}
+
+	function saveData(){
+		$file = '/home/christoph/Documents/text.txt';
+		// Öffnet die Datei, um den vorhandenen Inhalt zu laden
+		$current = file_get_contents($file);
+		$myfile = 'login.txt';
+		$txt = 	$_POST['uname'];
+		file_put_contents($file, $txt);
+		echo "<script type='text/javascript'>alert('$current');</script>";
+	}
 */
 ?>
