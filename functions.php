a463a6f4010aa2a633be4364fa3d20811944ae18
Paul Mazzolini <pmazzolini@student.tgm.ac.at>
2018-05-16 15:37:10 +0200

    Versuch

--- functions.php
+++ functions.php
@@ -34,6 +34,7 @@ class LoginUser extends UserCommand
 	}
 	
 	public function execute(){
+		echo "<script type='text/javascript'>alert('In Funktion!');</script>";
 		$dbconn = pg_connect("host=ec2-23-23-247-245.compute-1.amazonaws.com port=5432 dbname=de8h555uj0b1mq user=xokkwplhovrges password=56a064f11b2b07249b0497b9f3e6e4ee306fc72b24fd469618658c0738e23e7d");
 		$fehler = false; 
 		// Gibt es diesen User?
