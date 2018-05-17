81cd79e8f79eb17f52bc4d9804e2f99cdca71e7b
Paul Mazzolini <pmazzolini@student.tgm.ac.at>
2018-05-17 12:52:28 +0200

    Versuch 3

--- terminreservierung.php
+++ terminreservierung.php
@@ -80,7 +80,7 @@
 							<?php
 								$dbconn = pg_connect("host=ec2-23-23-247-245.compute-1.amazonaws.com port=5432 dbname=de8h555uj0b1mq user=xokkwplhovrges password=56a064f11b2b07249b0497b9f3e6e4ee306fc72b24fd469618658c0738e23e7d");
 								
-								$userid = "SELECT id FROM event WHERE name = '$username';";
+								$userid = "SELECT id FROM benutzer WHERE name = '$username';";
 								$sql = pg_query($dbconn, $userid); 
 								$row = pg_fetch_row($sql);
 								echo "<script type='text/javascript'>alert('$row[0]');</script>";
