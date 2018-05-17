a8186190fe40c63cfaf4dd0193fef5d6045337bf
Paul Mazzolini <pmazzolini@student.tgm.ac.at>
2018-05-17 13:03:37 +0200

    Funktioniert!!!

--- terminreservierung.php
+++ terminreservierung.php
@@ -88,14 +88,18 @@
 								$eventid = "SELECT event FROM teilnehmer WHERE usr = '$row[0]';";
 								$sql = pg_query($dbconn, $eventid);
 								$row = pg_fetch_row($sql);
-								echo "<script type='text/javascript'>alert('$row[0]');</script>";
+								/*echo "<script type='text/javascript'>alert('$row[0]');</script>";
 								echo "<script type='text/javascript'>alert('$row[1]');</script>";
-								echo "<script type='text/javascript'>alert('$row[2]');</script>";
+								echo "<script type='text/javascript'>alert('$row[2]');</script>";*/
 								
-								$eventname = "SELECT name FROM event WHERE id = '$row[0]';";
-								$sql = pg_query($dbconn, $eventname); 
-								$row = pg_fetch_row($sql);
-								echo "<script type='text/javascript'>alert('$row[0]');</script>";
+								for($i=0; $i < count($row); $i++) {
+									$eventname = "SELECT name FROM event WHERE id = '$row[$i]';";
+									$sql = pg_query($dbconn, $eventname); 
+									$row = pg_fetch_row($sql);
+									echo "<script type='text/javascript'>alert('$row[0]');</script>";
+								}
+								
+								
 								
 								
 								/*if($row[0] > 0) {
