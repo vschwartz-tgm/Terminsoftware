fae5a5dcb34a9717d1b07f5632274c868d381ccc
Paul Mazzolini <pmazzolini@student.tgm.ac.at>
2018-05-17 13:35:37 +0200

    Rechtschreibung

--- terminreservierung.php
+++ terminreservierung.php
@@ -77,7 +77,7 @@
 							<th>Eventname:</th><th>Ort:</th>
 						</tr>
 						<?php
-							// Vom aktuellen Benutzer alle Events aulesen und in die Tabelle schreiben
+							// Vom aktuellen Benutzer alle Events auslesen und in die Tabelle schreiben
 							
 							$dbconn = pg_connect("host=ec2-23-23-247-245.compute-1.amazonaws.com port=5432 dbname=de8h555uj0b1mq user=xokkwplhovrges password=56a064f11b2b07249b0497b9f3e6e4ee306fc72b24fd469618658c0738e23e7d");
 							
