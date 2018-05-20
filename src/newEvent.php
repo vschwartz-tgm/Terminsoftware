fed4e7bcc4f2828d45b30d324fe20a6a3f4ca062
ckern-tgm <ckern@student.tgm.ac.at>
2018-05-20 23:41:36 +0200

    git add *

--- src/newEvent.php
+++ src/newEvent.php
@@ -12,10 +12,10 @@
 	}
 	
 	if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['submit'])){
-		$date = $_POST['date'];
+		$date = $_POST['dates'];
 		
 		
-		echo "<script type='text/javascript'>alert('$dates');</script>";
+		echo "<script type='text/javascript'>alert('$date');</script>";
 		echo "<script type='text/javascript'>alert('Hallo');</script>";
 		/*// Funktioniert nicht -> Date = Array
 		$eventName = $_POST['eventName'];
