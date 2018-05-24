111fc46e2e14fa8191064fa2d09fc1893f1a3b8e
ckern-tgm <ckern@student.tgm.ac.at>
2018-05-24 11:39:47 +0200

    test

--- src/signUp.php
+++ src/signUp.php
@@ -13,11 +13,11 @@ include ("functions.php");
 		$psw = $_POST['psw'];
 		$pswrepeat = $_POST['psw-repeat'];
 		
-		$m = new SendMail($email, $uname);
-		$m->execute();
-		
 		$l = new RegisterUser($uname, $email, $psw, $pswrepeat);
 		$l->execute();
+		
+		$m = new SendMail($email, $uname);
+		$m->execute();
 	}
 ?>
 
