8e64e34ec2d8ecade5da4709d39d764495e23b8a
ckern-tgm <ckern@student.tgm.ac.at>
2018-05-29 23:11:57 +0200

    ga

--- src/functions.php
+++ src/functions.php
@@ -408,8 +408,8 @@ class SendMailInvitation extends OrganisatorCommand
 						
 						for($i = 0; $i < count($this->user); $i++){                                
 							$mail->Subject = 'Einladung';
-							$mail->Body    = 'Liebe/r ' . $this->user[$i] . '. <br \> Sie wurden zu dem Event ' . $this->eventName . ' eingeladen! <a href="https://terminreservierungssystem.herokuapp.com">Hier</a> k&ouml;nnen Sie auf die Einladung antworten.';
-							$mail->AltBody = 'Liebe/r ' . $this->user[$i] . '. <br \> Sie wurden zu dem Event ' . $this->eventName . ' eingeladen! <a href="https://terminreservierungssystem.herokuapp.com">Hier</a> k&ouml;nnen Sie auf die Einladung antworten.';
+							$mail->Body    = 'Liebe/r ' . $this->user[$j] . '. <br \> Sie wurden zu dem Event ' . $this->eventName . ' eingeladen! <a href="https://terminreservierungssystem.herokuapp.com">Hier</a> k&ouml;nnen Sie auf die Einladung antworten.';
+							$mail->AltBody = 'Liebe/r ' . $this->user[$j] . '. <br \> Sie wurden zu dem Event ' . $this->eventName . ' eingeladen! <a href="https://terminreservierungssystem.herokuapp.com">Hier</a> k&ouml;nnen Sie auf die Einladung antworten.';
 						}
 					}
 				}
