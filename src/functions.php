296f38982986c6a372ec7f0533ea7a479b1bfed7
ckern-tgm <ckern@student.tgm.ac.at>
2018-05-29 21:59:57 +0200

    test for loop with people

--- src/functions.php
+++ src/functions.php
@@ -402,11 +402,12 @@ class SendMailInvitation extends OrganisatorCommand
 					$mail->setFrom('terminreservierung.teamm@gmail.com', 'Terminreservierungsteam');
 					$mail->addAddress($email[0]);
 
-
-					$mail->isHTML(true);                                 
-					$mail->Subject = 'Einladung';
-					$mail->Body    = 'Liebe/r ' . $people . '. <br \> Sie wurden zu dem Event ' . $this->eventName . ' eingeladen! <a href="https://terminreservierungssystem.herokuapp.com">Hier</a> k&ouml;nnen Sie auf die Einladung antworten.';
-					$mail->AltBody = 'Liebe/r ' . $people . '. <br \> Sie wurden zu dem Event ' . $this->eventName . ' eingeladen! <a href="https://terminreservierungssystem.herokuapp.com">Hier</a> k&ouml;nnen Sie auf die Einladung antworten.';
+					for($i = 0; $i < count($people); i++){
+						$mail->isHTML(true);                                 
+						$mail->Subject = 'Einladung';
+						$mail->Body    = 'Liebe/r ' . $people[i] . '. <br \> Sie wurden zu dem Event ' . $this->eventName . ' eingeladen! <a href="https://terminreservierungssystem.herokuapp.com">Hier</a> k&ouml;nnen Sie auf die Einladung antworten.';
+						$mail->AltBody = 'Liebe/r ' . $people[i] . '. <br \> Sie wurden zu dem Event ' . $this->eventName . ' eingeladen! <a href="https://terminreservierungssystem.herokuapp.com">Hier</a> k&ouml;nnen Sie auf die Einladung antworten.';
+					}
 				}
 				$mail->send();
 
