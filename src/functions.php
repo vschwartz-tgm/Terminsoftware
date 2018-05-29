b02b853bc17ebadc9c037c63d864c2c7483f3824
ckern-tgm <ckern@student.tgm.ac.at>
2018-05-29 22:04:49 +0200

    test people[0]

--- src/functions.php
+++ src/functions.php
@@ -390,6 +390,7 @@ class SendMailInvitation extends OrganisatorCommand
 					$sql = pg_query($dbconn, $userMail); 
 					$email = pg_fetch_row($sql);
 
+
 					$mail->SMTPDebug = 0;                                
 					$mail->isSMTP();                                     
 					$mail->Host = 'smtp.gmail.com';  
@@ -401,13 +402,10 @@ class SendMailInvitation extends OrganisatorCommand
 
 					$mail->setFrom('terminreservierung.teamm@gmail.com', 'Terminreservierungsteam');
 					$mail->addAddress($email[0]);
-
-					for($i = 0; $i < count($people); i++){
-						$mail->isHTML(true);                                 
-						$mail->Subject = 'Einladung';
-						$mail->Body    = 'Liebe/r ' . $people[i] . '. <br \> Sie wurden zu dem Event ' . $this->eventName . ' eingeladen! <a href="https://terminreservierungssystem.herokuapp.com">Hier</a> k&ouml;nnen Sie auf die Einladung antworten.';
-						$mail->AltBody = 'Liebe/r ' . $people[i] . '. <br \> Sie wurden zu dem Event ' . $this->eventName . ' eingeladen! <a href="https://terminreservierungssystem.herokuapp.com">Hier</a> k&ouml;nnen Sie auf die Einladung antworten.';
-					}
+					$mail->isHTML(true);                                 
+					$mail->Subject = 'Einladung';
+					$mail->Body    = 'Liebe/r ' . $people[0] . '. <br \> Sie wurden zu dem Event ' . $this->eventName . ' eingeladen! <a href="https://terminreservierungssystem.herokuapp.com">Hier</a> k&ouml;nnen Sie auf die Einladung antworten.';
+					$mail->AltBody = 'Liebe/r ' . $people[0] . '. <br \> Sie wurden zu dem Event ' . $this->eventName . ' eingeladen! <a href="https://terminreservierungssystem.herokuapp.com">Hier</a> k&ouml;nnen Sie auf die Einladung antworten.';
 				}
 				$mail->send();
 
