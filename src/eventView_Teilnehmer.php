7cc17b2008c400ccbeb040a029f855383f517dc9
Paul Mazzolini <pmazzolini@student.tgm.ac.at>
2018-05-30 14:32:04 +0200

    Update place -> ort

--- src/eventView_Teilnehmer.php
+++ src/eventView_Teilnehmer.php
@@ -73,7 +73,7 @@
 						<td>
 							<?php
 								$dbconn = pg_connect("host=ec2-23-23-247-245.compute-1.amazonaws.com port=5432 dbname=de8h555uj0b1mq user=xokkwplhovrges password=56a064f11b2b07249b0497b9f3e6e4ee306fc72b24fd469618658c0738e23e7d");
-								$userid = "SELECT place FROM event WHERE name = '$eventname';";
+								$userid = "SELECT ort FROM event WHERE name = '$eventname';";
 								$sql = pg_query($dbconn, $userid); 
 								$row = pg_fetch_row($sql);
 								echo "$row[0]";
