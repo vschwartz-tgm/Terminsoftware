c67bfc7567b466018f9253b59b8fa3cfc985fbf1
Paul Mazzolini <pmazzolini@student.tgm.ac.at>
2018-05-31 21:16:31 +0200

    Ersteller und Dates eingefügt

--- src/eventView_Teilnehmer.php
+++ src/eventView_Teilnehmer.php
@@ -61,13 +61,27 @@
 						<th scope="col">Ort</th>
 						<th scope="col">Beschreibung</th>
 						<th scope="col">Teilnehmerliste</th>
+						<th scope="col">Ersteller</th>
 					</tr>
 				</thead>
 				<tbody>
 					<tr>
 						<td>
-							<p id="date"></p>
-							<button type="button" class="btn btn-outline-success" onclick="">Abstimmen</button>
+							<?php
+								// Dates in die Tabelle schreiben
+								$dbconn = pg_connect("host=ec2-23-23-247-245.compute-1.amazonaws.com port=5432 dbname=de8h555uj0b1mq user=xokkwplhovrges password=56a064f11b2b07249b0497b9f3e6e4ee306fc72b24fd469618658c0738e23e7d");
+								$eventid = "SELECT id FROM event WHERE name = '$eventname';";
+								$sql = pg_query($dbconn, $eventid);
+								$id = pg_fetch_row($sql);
+								
+								$userid = "SELECT date FROM datum WHERE eventid = '$id[0]';";
+								$sql = pg_query($dbconn, $userid); 
+								while ($row = pg_fetch_row($sql)) {
+									echo "$row[0]";
+									echo "<br />";
+								}
+							?>
+							<!--<button type="button" class="btn btn-outline-success" onclick="">Abstimmen</button>-->
 						</td>
 						<td>
 							<?php
@@ -107,6 +121,16 @@
 									echo "$name[0]";
 									echo "<br />";
 								}
+							?>
+						</td>
+						<td>
+							<?php
+								// Ersteller in die Tabelle schreiben
+								$dbconn = pg_connect("host=ec2-23-23-247-245.compute-1.amazonaws.com port=5432 dbname=de8h555uj0b1mq user=xokkwplhovrges password=56a064f11b2b07249b0497b9f3e6e4ee306fc72b24fd469618658c0738e23e7d");
+								$descr = "SELECT usr FROM event WHERE name = '$eventname';";
+								$sql = pg_query($dbconn, $descr); 
+								$row = pg_fetch_row($sql);
+								echo "$row[0]";
 							?>
 						</td>
 					</tr>
