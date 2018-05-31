a12cb67ea03dac40fde254d1009512a5adf26275
Paul Mazzolini <pmazzolini@student.tgm.ac.at>
2018-05-31 20:58:22 +0200

    Fehler ausgebessert

--- src/eventView_Teilnehmer.php
+++ src/eventView_Teilnehmer.php
@@ -93,8 +93,8 @@
 							<?php
 								// Teilnehmer in die Tabelle schreiben
 								$dbconn = pg_connect("host=ec2-23-23-247-245.compute-1.amazonaws.com port=5432 dbname=de8h555uj0b1mq user=xokkwplhovrges password=56a064f11b2b07249b0497b9f3e6e4ee306fc72b24fd469618658c0738e23e7d");
-								$eventid = "SELECT id FROM event WHERE name = '$username';";
-								$sql = pg_query($dbconn, $eventid); 
+								$eventid = "SELECT id FROM event WHERE name = '$eventname';";
+								$sql = pg_query($dbconn, $eventid);
 								$id = pg_fetch_row($sql);
 								
 								$userid = "SELECT usr FROM teilnehmer WHERE event = '$id[0]';";
