38b085c3d44d5e5d1462f15f45972a19ffaf7e16
Paul Mazzolini <pmazzolini@student.tgm.ac.at>
2018-06-01 11:13:20 +0200

    Text entfernt

--- src/eventView_Ersteller.php
+++ src/eventView_Ersteller.php
@@ -114,7 +114,6 @@
 								
 								$userid = "SELECT date FROM datum WHERE eventid = '$id[0]';";
 								$sql = pg_query($dbconn, $userid); 
-								echo "<p>Wählen Sie ihren Wunschtermin:</p>";
 								while ($row = pg_fetch_row($sql)) {
 									echo "$row[0]";
 									echo "  ";
