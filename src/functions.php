f0d79fa15f008736f914dc11108a152599b40f83
Paul Mazzolini <pmazzolini@student.tgm.ac.at>
2018-06-01 13:25:38 +0200

    ToDo weg

--- src/functions.php
+++ src/functions.php
@@ -508,8 +508,6 @@ class DeleteEingeladener extends OrganisatorCommand
 
     public function execute(){
         $dbconn = pg_connect("host=ec2-23-23-247-245.compute-1.amazonaws.com port=5432 dbname=de8h555uj0b1mq user=xokkwplhovrges password=56a064f11b2b07249b0497b9f3e6e4ee306fc72b24fd469618658c0738e23e7d");
-		
-		// ToDo: Zum Laufen bringen
         
         $uid = "SELECT id FROM benutzer WHERE name = '$this->username';";
         $sql = pg_query($dbconn, $uid);
