26554eca28f8365380e08fc80036b1fe4c08c7aa
ckern-tgm <ckern@student.tgm.ac.at>
2018-06-01 10:34:15 +0200

    rm event

--- src/functions.php
+++ src/functions.php
@@ -414,8 +414,6 @@ class DeleteEvent extends OrganisatorCommand
 
     public function execute(){
 
-        echo "<script type='text/javascript'>alert('Hallo');</script>";
-        /*
         $dbconn = pg_connect("host=ec2-23-23-247-245.compute-1.amazonaws.com port=5432 dbname=de8h555uj0b1mq user=xokkwplhovrges password=56a064f11b2b07249b0497b9f3e6e4ee306fc72b24fd469618658c0738e23e7d")     ;	
 
         $eid = "SELECT id FROM event WHERE name = '$this->event';";
@@ -440,7 +438,7 @@ class DeleteEvent extends OrganisatorCommand
 
         $rm = "DELETE FROM event WHERE name = '$this->event'; ";
         $sql = pg_query($dbconn, $rm);
-        header("Location: terminreservierung.php");*/
+        header("Location: terminreservierung.php");
     }
 }
 
