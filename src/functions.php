46456a0657333edc8c9e356fac35cde257abb29e
Paul Mazzolini <pmazzolini@student.tgm.ac.at>
2018-06-06 14:50:11 +0200

    Fehler auskommentiert

--- src/functions.php
+++ src/functions.php
@@ -303,13 +303,13 @@ class ChangeEvent extends OrganisatorCommand
 			}
 		} catch (Exception $e) {
 		}*/
-		
+		/*
 		// Werte updaten
 		if ($fehler == false){
 			
 			// ToDo: Funktioniert noch nicht ganz
 			$update = "UPDATE event SET name = '$this->nameNew' WHERE id = '$this->eventId';";
-			$sql = pg_query($dbconn, $update);
+			$sql = pg_query($dbconn, $update);*/
             /*
 			$update = "UPDATE event SET ort = '$this->ortNew' WHERE id = '$this->eventId';";
 			$sql = pg_query($dbconn, $update);
