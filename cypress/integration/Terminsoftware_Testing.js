06a95820f580355d8475385a1630aa163166dfec
mwintersperger-tgm <mwintersperger@student.tgm.ac.at>
2018-06-07 20:19:27 +0200

    reorganized the tests

--- cypress/integration/Terminsoftware_Testing.js
+++ cypress/integration/Terminsoftware_Testing.js
@@ -8,14 +8,14 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 })
 
 describe('Navigating the Sites', function() {
-	it.skip("Go to the register site", function() {
+	it("Go to the register site", function() {
 		cy.visit('https://terminreservierungssystem.herokuapp.com/src/login.php')
 		
 		cy.get('[href="signUp.php"]').first().click()
 		
 		cy.url().should('include', '/src/signUp.php')
 	})
-	it.skip("Leave the register site on cancel", function() {
+	it("Leave the register site on cancel", function() {
 		cy.visit('https://terminreservierungssystem.herokuapp.com/src/login.php')
 		
 		cy.get('[href="signUp.php"]').first().click()
@@ -24,7 +24,7 @@ describe('Navigating the Sites', function() {
 
 		cy.url().should('include', '/src/login.php')
 	})
-	it.skip("Stay on the register site on Sign Up with empty Textfields", function() {
+	it("Stay on the register site on Sign Up with empty Textfields", function() {
 		cy.visit('https://terminreservierungssystem.herokuapp.com/src/login.php')
 		
 		cy.get('[href="signUp.php"]').first().click()
@@ -47,7 +47,7 @@ describe('Navigating the Sites', function() {
 
 		cy.url().should('include', '/src/login.php')
 	})
-	it.skip("Stay on the register site on Sign up with existing user", function() {
+	it("Stay on the register site on Sign up with existing user", function() {
 		cy.visit('https://terminreservierungssystem.herokuapp.com/src/login.php')
 		
 		cy.get('a').contains('Sign Up').first().click()
@@ -61,7 +61,7 @@ describe('Navigating the Sites', function() {
 
 		cy.url().should('include', 'src/signUp.php')
 	})
-	it.skip("Enter main site via successful login", function() {
+	it("Enter main site via successful login", function() {
 		cy.visit('https://terminreservierungssystem.herokuapp.com/src/login.php')
 
 		cy.get('[name="uname"]').first().type('Tester')
@@ -71,14 +71,14 @@ describe('Navigating the Sites', function() {
 
 		cy.url().should('include', '/src/terminreservierung.php')
 	})
-	it.skip("Stay on login site via failed login", function() {
+	it("Stay on login site via failed login", function() {
 		cy.visit('https://terminreservierungssystem.herokuapp.com/src/login.php')
 		
 		cy.get('button').contains('Login').first().click()
 
 		cy.url().should('include', '/src/login.php')
 	})
-	it.skip("Enter searchView", function() {
+	it("Enter searchView", function() {
 		cy.visit('https://terminreservierungssystem.herokuapp.com/src/login.php')
 		
 		cy.get('[name="uname"]').first().type('Tester')
@@ -91,7 +91,7 @@ describe('Navigating the Sites', function() {
 		
 		cy.url().should('include', '/src/searchView.php')
 	})
-	it.skip("Stay on main site via failed search", function() {
+	it("Stay on main site via failed search", function() {
 		cy.visit('https://terminreservierungssystem.herokuapp.com/src/login.php')
 
 		cy.get('[name="uname"]').first().type('Tester')
@@ -105,12 +105,12 @@ describe('Navigating the Sites', function() {
 		
 		cy.url().should('include', '/src/terminreservierung.php')
 	})
-	it.skip("Have to be logged in to enter main site", function() {
+	it("Have to be logged in to enter main site", function() {
 		cy.visit('https://terminreservierungssystem.herokuapp.com/src/terminreservierung.php')
 
 		cy.get('a').contains('einloggen')
 	})
-	it.skip("Leave main site via logout", function() {
+	it("Leave main site via logout", function() {
 		cy.visit('https://terminreservierungssystem.herokuapp.com/src/login.php')
 
 		cy.get('[name="uname"]').first().type('Tester')
@@ -122,7 +122,7 @@ describe('Navigating the Sites', function() {
 		
 		cy.url().should('include', '/src/login.php')
 	})
-	it.skip("Leave searchView via logout", function() {
+	it("Leave searchView via logout", function() {
 		cy.visit('https://terminreservierungssystem.herokuapp.com/src/login.php')
 
 		cy.get('[name="uname"]').first().type('Tester')
@@ -137,7 +137,7 @@ describe('Navigating the Sites', function() {
 		
 		cy.url().should('include', '/src/login.php')
 	})
-	it.skip("Enter newEvent", function() {
+	it("Enter newEvent", function() {
 		cy.visit('https://terminreservierungssystem.herokuapp.com/src/login.php')
 
 		cy.get('[name="uname"]').first().type('Tester')
@@ -149,7 +149,7 @@ describe('Navigating the Sites', function() {
 		
 		cy.url().should('include', '/src/newEvent.php')
 	})
-	it.skip("leave newEvent via logout", function() {
+	it("leave newEvent via logout", function() {
 		cy.visit('https://terminreservierungssystem.herokuapp.com/src/login.php')
 
 		cy.get('[name="uname"]').first().type('Tester')
@@ -162,7 +162,7 @@ describe('Navigating the Sites', function() {
 		cy.get('button').contains('Logout').first().click()
 		cy.url().should('include', '/src/login.php')
 	})
-	it.skip("leave newEvent via cancel", function() {
+	it("leave newEvent via cancel", function() {
 		cy.visit('https://terminreservierungssystem.herokuapp.com/src/login.php')
 
 		cy.get('[name="uname"]').first().type('Tester')
@@ -175,7 +175,7 @@ describe('Navigating the Sites', function() {
 		cy.get('button').contains('Abbrechen').first().click()
 		cy.url().should('include', '/src/terminreservierung.php')
 	})
-	it.skip("Stay on newEvent via failed add", function() {
+	it("Stay on newEvent via failed add", function() {
 		cy.visit('https://terminreservierungssystem.herokuapp.com/src/login.php')
 
 		cy.get('[name="uname"]').first().type('Tester')
@@ -185,12 +185,11 @@ describe('Navigating the Sites', function() {
 		
 		cy.get('button').contains('Add Event').first().click()
 		
-
 		cy.get('[id="submit"]').click()
 
 		cy.url().should('include', '/src/newEvent.php')
 	})
-	it.skip("Leave newEvent via successful add", function() {
+	it("Leave newEvent via successful add", function() {
 		cy.visit('https://terminreservierungssystem.herokuapp.com/src/login.php')
 
 		cy.get('[name="uname"]').first().type('Tester')
@@ -200,7 +199,7 @@ describe('Navigating the Sites', function() {
 		
 		cy.get('button').contains('Add Event').first().click()
 		
-		cy.get('[id="eventName"]').type('testblabla')
+		cy.get('[id="eventName"]').type('testblablabla')
 		cy.get('[id="date"]').type('01.01.2001 10:10')
 		cy.get('[id="location"]').type('testing location')
 		cy.get('[id="desc"]').type('ein test')
@@ -210,7 +209,40 @@ describe('Navigating the Sites', function() {
 
 		cy.url().should('include', '/src/terminreservierung.php')
 	})
-	it.skip("Stay on newEvent via adding an existing event", function() {
+	it("Stay on newEvent via adding an existing event", function() {
+		cy.visit('https://terminreservierungssystem.herokuapp.com/src/login.php')
+
+		cy.get('[name="uname"]').first().type('Tester')
+		cy.get('[name="psw"]').first().type('tests')
+		
+		cy.get('button').contains('Login').first().click()
+		
+		cy.get('button').contains('Add Event').first().click()
+		
+		cy.get('[id="eventName"]').type('testblablabla')
+		cy.get('[id="date"]').type('01.01.2001 10:10')
+		cy.get('[id="location"]').type('testing location')
+		cy.get('[id="desc"]').type('ein test')
+		cy.get('[id="people[]"]').type('tester')
+		
+		cy.get('[id="submit"]').click()
+		
+		cy.get('button').contains('Add Event').first().click()
+		
+		cy.get('[id="eventName"]').type('testblabla')
+		cy.get('[id="date"]').type('01.01.2001 10:10')
+		cy.get('[id="location"]').type('testing location')
+		cy.get('[id="desc"]').type('ein test')
+		cy.get('[id="people[]"]').type('tester')
+		
+		cy.get('[id="submit"]').click()
+		cy.url().should('include', '/src/newEvent.php')
+	})
+
+})
+
+describe('Event', function() {
+	it("add a new Event", function() {
 		cy.visit('https://terminreservierungssystem.herokuapp.com/src/login.php')
 
 		cy.get('[name="uname"]').first().type('Tester')
@@ -227,10 +259,8 @@ describe('Navigating the Sites', function() {
 		cy.get('[id="people[]"]').type('tester')
 		
 		cy.get('[id="submit"]').click()
-
-		cy.url().should('include', '/src/newEvent.php')
 	})
-	it.skip("Add another date textfield via pressing the add date textfields button", function() {
+	it("Add another date textfield via pressing the add date textfields button", function() {
 		cy.visit('https://terminreservierungssystem.herokuapp.com/src/login.php')
 
 		cy.get('[name="uname"]').first().type('Tester')
@@ -244,7 +274,7 @@ describe('Navigating the Sites', function() {
 		
 		cy.get('[id="date2"]')
 	})
-	it.skip("Add another date textfield via pressing the add date textfield button and then removing it via the remove date textfield button", function() {
+	it("Add another date textfield via pressing the add date textfield button and then removing it via the remove date textfield button", function() {
 		cy.visit('https://terminreservierungssystem.herokuapp.com/src/login.php')
 
 		cy.get('[name="uname"]').first().type('Tester')
@@ -259,7 +289,7 @@ describe('Navigating the Sites', function() {
 		cy.get('button').contains('-').first().click()
 		cy.get('[id="date2"]').should('not.exist')
 	})
-	it.skip("pressing the remove date textfield button can not reduce the number of date textfields to 0", function() {
+	it("pressing the remove date textfield button can not reduce the number of date textfields to 0", function() {
 		cy.visit('https://terminreservierungssystem.herokuapp.com/src/login.php')
 
 		cy.get('[name="uname"]').first().type('Tester')
@@ -273,7 +303,7 @@ describe('Navigating the Sites', function() {
 		
 		cy.get('[id="date"]')
 	})
-	it.skip("Add another people textfield via pressing the add people textfields button", function() {
+	it("Add another people textfield via pressing the add people textfields button", function() {
 		cy.visit('https://terminreservierungssystem.herokuapp.com/src/login.php')
 
 		cy.get('[name="uname"]').first().type('Tester')
@@ -287,7 +317,7 @@ describe('Navigating the Sites', function() {
 		
 		cy.get('[id="people2"]')
 	})
-	it.skip("Add another people textfield via pressing the add people textfield button and then removing it via the remove people textfield button", function() {
+	it("Add another people textfield via pressing the add people textfield button and then removing it via the remove people textfield button", function() {
 		cy.visit('https://terminreservierungssystem.herokuapp.com/src/login.php')
 
 		cy.get('[name="uname"]').first().type('Tester')
@@ -303,7 +333,7 @@ describe('Navigating the Sites', function() {
 		
 		cy.get('[id="people2"]').should('not.exist')
 	})
-	it.skip("pressing the remove people textfield button can not reduce the number of people textfields to 0", function() {
+	it("pressing the remove people textfield button can not reduce the number of people textfields to 0", function() {
 		cy.visit('https://terminreservierungssystem.herokuapp.com/src/login.php')
 
 		cy.get('[name="uname"]').first().type('Tester')
@@ -334,8 +364,8 @@ describe('Navigating the Sites', function() {
 		cy.get('[id="people[]"]').type('tester')
 		
 		cy.get('[id="submit"]').click()
-
-		cy.get('[href ="terminreservierung.php?erstEvent=testblabla"]').contains('testblabla')
+		cy.once
+		cy.get('[href ="terminreservierung.php?erstEvent=testblabla"]')
 	})
 	it("Can look at Event by klicking on it", function() {
 		cy.visit('https://terminreservierungssystem.herokuapp.com/src/login.php')
@@ -377,5 +407,34 @@ describe('Navigating the Sites', function() {
 		cy.get('[value="Event löschen"]').click()
 		
 		cy.get('[href ="terminreservierung.php?erstEvent=testblabla"]').should('not.exist')
+	})
+})
+describe('search', function() {
+	it("searchView shows searched events", function() {
+		cy.visit('https://terminreservierungssystem.herokuapp.com/src/login.php')
+		
+		cy.get('[name="uname"]').first().type('Tester')
+		cy.get('[name="psw"]').first().type('tests')
+		
+		cy.get('button').contains('Login').first().click()
+		
+		cy.get('input').first().type('test')
+		cy.get('button').contains('Search').first().click()
+		
+		cy.get('a').contains('test')
+	})
+	it("searchView shows searched user", function() {
+		cy.visit('https://terminreservierungssystem.herokuapp.com/src/login.php')
+		
+		cy.get('[name="uname"]').first().type('Tester')
+		cy.get('[name="psw"]').first().type('tests')
+		
+		cy.get('button').contains('Login').first().click()
+		
+		cy.get('label').contains('User').click()
+		cy.get('input').first().type('Tester')
+		cy.get('button').contains('Search').first().click()
+		
+		cy.get('td').contains('Tester')
 	})
 })
