d5933dd168f852fd498c586096517158df12ce79
vschwartz-tgm <vschwartz@student.tgm.ac.at>
2018-06-08 12:03:32 +0200

    potential fix

--- src/eventView_Teilnehmer.php
+++ src/eventView_Teilnehmer.php
@@ -15,12 +15,14 @@
 	}
 	$eventname = $_SESSION['teilnehmerEvent'];
 
+	$eventid = "SELECT id from event where name = '$eventname'";
+	$userid = "SELECT id from usr where name = '$username'";
+
 	if(isset($_POST['submit'])){
 	    $commentContent = $_POST['commentField'];
+	    $c = new createComment($eventid, $commentContent, $userid);
+		$c->execute();
     }
-
-    $eventid = "SELECT id from event where name = '$eventname'";
-	$userid = "SELECT id from usr where name = '$username'";
 
 	
 	// Logout
@@ -174,7 +176,7 @@
 			<form method="post">
 				<div class="form-group">
 					<input type="text" placeholder="Kommentar" name="commentField" class="form-control" id="commentField" />
-					<button type="submit" class="btn btn-outline-dark form-control" onclick="<?php $c = new createComment($eventid, $commentContent, $userid); $c->execute() ?>" name="commentBtn" id="commentBtn" style="float: right;">Posten</button>
+					<button type="submit" class="btn btn-outline-dark form-control" name="commentBtn" id="commentBtn" style="float: right;">Posten</button>
 				</div>
 				<table class="table scroll">
 					<thead>
--- src/functions.php
+++ src/functions.php
@@ -736,7 +736,7 @@ class createComment extends UserCommand{
 
     function execute() {
         $dbconn = pg_connect("host=ec2-23-23-247-245.compute-1.amazonaws.com port=5432 dbname=de8h555uj0b1mq user=xokkwplhovrges password=56a064f11b2b07249b0497b9f3e6e4ee306fc72b24fd469618658c0738e23e7d");
-        echo "<script type='text/javascript'>alert('$this->eventId, $this->userId, $this->comment');</script>";
+        echo "<script type='text/javascript'>alert('$this->eventId, $this->comment, $this->userId');</script>";
         /*
         $addComment = "insert into kommentar(event, comment, usr) values('$this->eventId','$this->comment','$this->userId')";
         $cmnt = pg_query($dbconn, $addComment);
@@ -762,7 +762,7 @@ class Comment extends UserCommand {
 
     function execute() {
         $dbconn = pg_connect("host=ec2-23-23-247-245.compute-1.amazonaws.com port=5432 dbname=de8h555uj0b1mq user=xokkwplhovrges password=56a064f11b2b07249b0497b9f3e6e4ee306fc72b24fd469618658c0738e23e7d");
-        $usr = "SELECT * FROM event WHERE id = ''";
+        
     }
 
 }
