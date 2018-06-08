2d06b729676223092546c38e911122871b5b55a1
vschwartz-tgm <vschwartz@student.tgm.ac.at>
2018-06-08 12:09:51 +0200

    fix?

--- src/functions.php
+++ src/functions.php
@@ -728,13 +728,13 @@ class createComment extends UserCommand{
     private $userId;
     private $comment;
 
-    function __construct($eventId, $comment, $userId) {
+    public function __construct($eventId, $comment, $userId) {
         $this->eventId = $eventId;
         $this->comment = $comment;
         $this->userId = $userId;
     }
 
-    function execute() {
+    public function execute() {
         $dbconn = pg_connect("host=ec2-23-23-247-245.compute-1.amazonaws.com port=5432 dbname=de8h555uj0b1mq user=xokkwplhovrges password=56a064f11b2b07249b0497b9f3e6e4ee306fc72b24fd469618658c0738e23e7d");
         echo "<script type='text/javascript'>alert('this->eventId, this->comment, this->userId');</script>";
         /*
