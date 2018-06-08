a24ecd9e55aa961c110ead3b0719a70840dbc9b9
vschwartz-tgm <vschwartz@student.tgm.ac.at>
2018-06-08 12:49:56 +0200

    ..

--- src/eventView_Teilnehmer.php
+++ src/eventView_Teilnehmer.php
@@ -16,6 +16,7 @@
 	$eventname = $_SESSION['teilnehmerEvent'];
 
 	if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['commentBtn'])){
+        $dbconn = pg_connect("host=ec2-23-23-247-245.compute-1.amazonaws.com port=5432 dbname=de8h555uj0b1mq user=xokkwplhovrges password=56a064f11b2b07249b0497b9f3e6e4ee306fc72b24fd469618658c0738e23e7d");
         $eventid = "SELECT id from event where name = '$eventname'";
         $sqleventid = pg_query($dbconn, $eventid);
         $evntid = pg_fetch_row($sqleventid);
