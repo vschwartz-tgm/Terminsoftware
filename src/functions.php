be52e8665c686c81bc5f982adc3313d316542ffd
vschwartz-tgm <vschwartz@student.tgm.ac.at>
2018-06-11 14:25:09 +0200

    comment abgeschlossen

--- src/functions.php
+++ src/functions.php
@@ -672,8 +672,6 @@ class CommentEvent extends EventCommand
 
     public function execute() {
         $dbconn = pg_connect("host=ec2-23-23-247-245.compute-1.amazonaws.com port=5432 dbname=de8h555uj0b1mq user=xokkwplhovrges password=56a064f11b2b07249b0497b9f3e6e4ee306fc72b24fd469618658c0738e23e7d");
-        
-		// ToDo: Event Command in die DB schreiben
 		
 		$eventid = "SELECT id from event where name = '$this->eventname'";
         $sqleventid = pg_query($dbconn, $eventid);
