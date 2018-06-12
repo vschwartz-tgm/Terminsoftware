50782f9bb3b115fade4c051927eb967ed24dd2f8
Paul Mazzolini <pmazzolini@student.tgm.ac.at>
2018-06-12 08:09:09 +0200

    Kommentar versuch

--- src/eventView_Teilnehmer.php
+++ src/eventView_Teilnehmer.php
@@ -184,20 +184,20 @@
 						</tr>
 					</thead>
 					<tbody>
-						<?php /*
+						<?php
 						$dbconn = pg_connect("host=ec2-23-23-247-245.compute-1.amazonaws.com port=5432 dbname=de8h555uj0b1mq user=xokkwplhovrges password=56a064f11b2b07249b0497b9f3e6e4ee306fc72b24fd469618658c0738e23e7d");
 
-        				$eventid = "SELECT id from event where name = '$this->eventname'";
+        				$eventid = "SELECT id from event where name = '$eventname'";
         				$sqleventid = pg_query($dbconn, $eventid);
         				$evntid = pg_fetch_row($sqleventid);
 
-        				$commentSELECT = "SELECT comment from event where id = '$evntid[0]'";
+        				$commentSELECT = "SELECT comment from kommentar where event = '$evntid[0]'";
         				$sqlcomment = pg_query($dbconn, $commentSELECT);
-        				$comment = pg_fetch_row($sqlcomment); */
+        				$comment = pg_fetch_row($sqlcomment);
         				?>
-
-        				<td>Paul<?php //echo $username; ?></td>
-        				<td>TOlles Event<?php //echo $comment[0]; ?></td>
+						
+        				<td><?php echo $username; ?></td>
+        				<td><?php echo $comment[0]; ?></td>
 					</tbody>
 				</table>
 			</form>
